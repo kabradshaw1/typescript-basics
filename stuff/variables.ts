31626345a24408e01000dd51d483392fe2597283
kabradshaw1 <itllbok82@gmail.com>
2023-03-21 09:24:31 -0400

    boring stuff

---
+++ stuff/variables.ts
@@ -0,0 +1,17 @@
+let color: string[] = ['red', 'green', 'blue']
+
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// object literial
+let point: { x: number, y: number} = {
+  x: 10,
+  y: 20
+}
+
+const logNumber: (i:number) => void = (i: number) => {
+  console.log(i)
+}
